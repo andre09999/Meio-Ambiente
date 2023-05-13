19185ca3789f04583311f85f49788179af02fe2a
andre09999 <andre09999@live.com>
2023-05-13 20:09:30 -0300

    a

--- src/pages/Artigos.jsx
+++ src/pages/Artigos.jsx
@@ -1,10 +1,10 @@
 import './artigos.css';
-import arquivo from './arquivos/RESUMO TRABALHO DE EXTENSÃO.pdf'
+import arquivo from './arquivos/FOLDER.pdf'
 
 function Artigos() {
   return (
     <main id='artigos'>
-     <a href={arquivo} download className='linkArtigos'> Clique para abrir o Resumo trabalho de extensão </a>
+     <a href={ arquivo } download className='linkArtigos'> Clique para abrir o Resumo trabalho de extensão </a>
     </main>
   );
 }
