bbc202092346c1f5bf0a034c7b4179cbdeb6ccc1
andre09999 <andre09999@live.com>
2023-05-24 15:33:04 -0300

    update

--- src/pages/Artigos.jsx
+++ src/pages/Artigos.jsx
@@ -1,10 +1,12 @@
 import './artigos.css';
 import arquivo from './arquivos/RESUMO TRABALHO DE EXTENSÃO.pdf'
+import folder from './arquivos/novo11_merged.pdf'
 
 function Artigos() {
   return (
     <main id='artigos'>
      <a href={ arquivo } download className='linkArtigos'> Clique para abrir o Resumo trabalho de extensão </a>
+     <a href={ folder } download className='linkArtigos'> Folder </a>
     </main>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <main id='home'>
      <h1 className='tituloHome'>TRABALHO DE EXTENSÃO</h1>
      <h2>Direito Ambiental, Previdenciário e Tributário</h2>
-     <h3 className='subTIuloHome'>GRUPO DE LEITURA 1 </h3>
+
      <h3>OS DESASTRES AMBIENTAIS E SUAS (IN)JUSTIÇAS</h3>
     </main>
   );
